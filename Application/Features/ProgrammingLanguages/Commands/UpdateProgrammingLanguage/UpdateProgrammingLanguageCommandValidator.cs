86370fc987063cf9cfe2d0857bc6664da2bb2f79
Sinan Kara <snan_kara_@hotmail.com>
2022-09-05 22:15:04 +0300

    Add project files.

---
+++ Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommandValidator: AbstractValidator<UpdateProgrammingLanguageCommand>
+    {
+        public UpdateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(l => l.Name).NotEmpty();
+        }
+    }
+}
